03be447709d69a6150816c05accd92db641f27de
Glinberg, Mark Nathaniel <mglinberg@gatech.edu>
2022-06-23 10:50:24 +0200

    Created Person
    Person class for any person created

---
+++ src/Person.java
@@ -0,0 +1,44 @@
+package src;
+
+import java.util.Date;
+import java.util.TreeSet;
+
+public class Person {
+    private String username;
+    private String fname;
+    private String lname;
+    private Date bdate;
+    private String address;
+    private TreeSet<DeliveryService> workingFor;
+    private DeliveryService manages;
+
+    public Person(String username, String fname, String lname, int year, int month, int date, String address) {
+        this.username = username;
+        this.fname = fname;
+        this.lname = lname;
+        this.bdate = new Date(year, month, date);
+        this.address = address;
+        workingFor = new TreeSet<DeliveryService>();
+        manages = null;
+    }
+
+    public void workFor(DeliveryService service) throws Exception {
+
+    }
+
+    public void leave(DeliveryService service) throws Exception {
+
+    }
+
+    public void becomeManager(DeliveryService service) throws Exception {
+
+    }
+
+    public Pilot becomePilot(String license, int experience) throws Exception {
+
+    }
+
+    public String toString() {
+
+    }
+}
